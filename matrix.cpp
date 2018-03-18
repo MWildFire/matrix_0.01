dac6763a36b171c0f4bacfed54ccb1ef09796450
MWildFire <mvstrike17@gmail.com>
2018-03-18 20:52:08 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -6,6 +6,14 @@
 //  Copyright © 2018 Сурков Дмитрий Валерьевич. All rights reserved.
 //
 
+//
+//  matrix.cpp
+//  Matrox_0.0.1
+//
+//  Created by Сурков Дмитрий Валерьевич on 26.02.2018.
+//  Copyright © 2018 Сурков Дмитрий Валерьевич. All rights reserved.
+//
+
 #include "matrix.hpp"
 #include <assert.h>
 #include <fstream>
@@ -20,14 +28,14 @@ matrix_t::matrix_t (std::size_t collumns, std::size_t rows)
     elements_ = new float *[rows_];
     for (std::size_t i = 0; i < rows_; i++)
     {
-        elemens_[i] =  new float *[collumns_];
+        elements_[i] =  new float [collumns_];
         for (std::size_t j = 0; j < collumns_; j ++)
         {
             elements_[i][j] = 0;
         }
     }
-    this -> rows = rows_
-    this -> collumns = collumns;
+    this -> rows_ = rows;
+    this -> collumns_ = collumns;
 }
 
 matrix_t::matrix_t( matrix_t const & other )
@@ -75,7 +83,7 @@ matrix_t matrix_t::sub( matrix_t const &other)
         {
             result.elements_[i][j] = elements_[i][j] - other.elements_[i][j];
         }
-
+        
     }
     result.rows_ = rows_;
     result.collumns_ = collumns_;
@@ -113,7 +121,7 @@ matrix_t matrix_t::mult( matrix_t const &other)
 
 matrix_t matrix_t::trans()
 {
-    matrix_t result(rows_; collumns_);
+    matrix_t result(rows_, collumns_);
     for (int i = 0; i < result.rows_; i++)
     {
         for (int j = 0; j < result.collumns_; j++)
@@ -158,40 +166,40 @@ std::ifstream & matrix_t::read( std::ifstream & file )
                     }
                 }
             }
-        
-        if( success )
-        {
-            for( std::size_t i = 0; i < rows_; ++i )
-            {
-                delete [] elements_[ i ];
-            }
-            delete [] elements_;
             
-            rows_ = rows;
-            collumns_ = collumns;
-            elements_ = elements;
+            if( success )
+            {
+                for( std::size_t i = 0; i < rows_; ++i )
+                {
+                    delete [] elements_[ i ];
+                }
+                delete [] elements_;
+                
+                rows_ = rows;
+                collumns_ = collumns;
+                elements_ = elements;
+            }
+            else
+            {
+                for( std::size_t i = 0; i < rows; ++i )
+                {
+                    delete [] elements[ i ];
+                }
+                delete [] elements;
+            }
         }
         else
         {
-            for( std::size_t i = 0; i < rows; ++i )
-            {
-                delete [] elements[ i ];
-            }
-            delete [] elements;
+            success = false;
         }
+        if( !success )
+        {
+            sstream.setstate( std::ios_base::failbit );
+        }
+        return file;
     }
-    else
-    {
-        success = false;
-    }
-    if( !success )
-    {
-        sstream.setstate( std::ios_base::failbit );
-    }
-    return file;
-}
     
-
+    
 std::ostream & matrix_t::write( std::ostream & stream ) const
 {
     stream << rows_ << ", " << collumns_;
